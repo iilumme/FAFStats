93055724e35c74774fa488f38cc9f8757b1665ec
Blodir <Amwrailth@gmail.com>
2016-04-30 12:23:23 +0300

    Add player rating to game_show

--- app/assets/javascripts/controllers/GameController.js
+++ app/assets/javascripts/controllers/GameController.js
@@ -1,14 +1,25 @@
 FAFStats.controller('GameController', function ($scope, $http, $routeParams, FAFApi) {
 
   $scope.gameid = $routeParams.id;
+  $scope.players = new Array();
 
   var renderGame = function(id) {
     FAFApi.getGame(id).success(function(gamedata){
       $scope.game = gamedata.data[0].attributes;
       $scope.start_time = new Date($scope.game.start_time).toString();
-      console.log();
       $scope.game_length = Math.floor((new Date($scope.game.players[0].score_time)
       - new Date($scope.game.start_time)) / (1000*60));
+
+      for (var i = 0; i < $scope.game.players.length; i++) {
+        getPlayer($scope.game.players[i].player_id);
+      }
+
+    });
+  }
+
+  var getPlayer = function(id) {
+    FAFApi.findPlayer(id).success(function(player){
+      $scope.players.push(player);
     });
   }
 
