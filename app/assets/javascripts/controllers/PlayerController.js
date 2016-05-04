c013ac1f246ef189879c3e0f02c9ac0dd5428c1e
Blodir <amwrailth@gmail.com>
2016-05-04 17:40:00 +0300

    Add dynamic ratingevolution min and max ticks

--- app/assets/javascripts/controllers/PlayerController.js
+++ app/assets/javascripts/controllers/PlayerController.js
@@ -53,6 +53,8 @@ FAFStats.controller('PlayerController', function ($scope, $routeParams, FAFApi, 
 
     StatsApi.getPlayersRatings(playerID).success(function(ratings){
 
+      var ratingData = getRatings(ratings);
+
       var data = {
         labels: getMonthLabels(),
         datasets: [
@@ -68,7 +70,7 @@ FAFStats.controller('PlayerController', function ($scope, $routeParams, FAFApi, 
             pointHoverBackgroundColor: "rgba(220,220,220,1)",
             pointHoverBorderColor: "rgba(220,220,220,1)",
             pointHoverBorderWidth: 2,
-            data: getRatings(ratings)
+            data: ratingData
           }
         ]
       };
@@ -77,8 +79,8 @@ FAFStats.controller('PlayerController', function ($scope, $routeParams, FAFApi, 
         scales: {
           yAxes: [{
             ticks: {
-              min: 500,
-              max: 2500
+              min: Math.min.apply(Math, ratingData) - 100,
+              max: Math.max.apply(Math, ratingData) + 100
             }
           }]
         }
