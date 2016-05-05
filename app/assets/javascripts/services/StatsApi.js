e9fe10682aa7e2f2acaa7d9597523a2b3ed52a64
Blodir <amwrailth@gmail.com>
2016-05-05 15:37:20 +0300

    Refactor getUser

--- app/assets/javascripts/services/StatsApi.js
+++ app/assets/javascripts/services/StatsApi.js
@@ -1,8 +1,12 @@
 FAFStats.service('StatsApi', function ($http) {
 
-  this.getUser = function() {
-    return $http.get('/users/1.json');
+  this.getUser = function(playerID) {
+    return $http.get('/users/2.json');
   };
+
+  this.getSession = function() {
+    return $http.get('/users/1.json');
+  }
 
   this.getPlayersRatings = function(player_id) {
     return $http.get('/ratings/player/' + player_id + '.json')
