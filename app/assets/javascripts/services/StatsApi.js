5d7b385d7dee3f76344caacc0e553cc580a36f83
iilumme <iilumme@cs.helsinki.fi>
2016-05-06 16:32:01 +0300

    fix

--- app/assets/javascripts/services/StatsApi.js
+++ app/assets/javascripts/services/StatsApi.js
@@ -8,7 +8,7 @@ FAFStats.service('StatsApi', function ($http) {
 
   // Returns the user in the session OR id:0, player_id:0
   this.getSession = function() {
-    return $http.get('/sessions/current_user')
+    return $http.get('/sessions/current_user.json')
   }
 
   this.getPlayersRatings = function(player_id) {
