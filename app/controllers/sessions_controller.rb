f69ea707f6844538e45875fa3b1ce76cf8288b6c
iilumme <iilumme@cs.helsinki.fi>
2016-05-14 17:28:15 +0300

    game

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,26 +2,31 @@ class SessionsController < ApplicationController
 
   def create_oauth
 
-    #raise request.env["omniauth.auth"].to_yaml
+    raise request.env['omniauth.auth'].to_yaml
 
-    user = User.find_by_player_id(env["omniauth.auth"].info.id)
-
-    if user.nil?
-      user = User.create
-      user.player_id = env["omniauth.auth"].info.id
-      user.usertype = 2
-      user.save
-      session[:user_id] = user.id
-
-    elsif user && user.usertype != 3
-      session[:user_id] = user.id
-
-    elsif user.usertype == 3
-      redirect_to :root
-    end
+    # user = User.find_by_player_id(env["omniauth.auth"].info.id)
+    #
+    # if user.nil?
+    #   user = User.create
+    #   user.player_id = env["omniauth.auth"].info.id
+    #   user.usertype = 2
+    #   user.save
+    #   session[:user_id] = user.id
+    #
+    # elsif user && user.usertype != 3
+    #   session[:user_id] = user.id
+    #
+    # elsif user.usertype == 3
+    #   redirect_to :root
+    # end
 
   end
 
+  # POST /login
+  # TEMPORARY LOGIN
+  # Returns:
+  #  information of user in session
+  #  or 422 (no user found)
   def create
     @user = User.find_by player_id: params[:player_id]
 
@@ -35,6 +40,7 @@ class SessionsController < ApplicationController
   end
 
   # GET /logout
+  # Returns HTTP status 200
   def destroy
     session[:user_id] = nil
     render :nothing => true, :status => :ok
